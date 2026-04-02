51102a86864663f0d5eae8ad714bf383b96f0090
LuisAguiarGIT <luis.aguiar23@outlook.com>
2026-04-02 19:19:31 +0100

    Navigation icon logic

--- src/components/NavIcon.tsx
+++ src/components/NavIcon.tsx
@@ -4,12 +4,15 @@ import type { LucideIcon } from 'lucide-react';
 interface INavIconProps {
   icon: LucideIcon;
   route: string;
+  path: string;
 }
 
-export default function NavIcon({ icon: Icon, route }: INavIconProps) {
+export default function NavIcon({ icon: Icon, route, path }: INavIconProps) {
   return (
     <Link href={route}>
-      <Icon className="w-6 h-6 stroke-gray-500 mt-6" />
+      <Icon
+        className={`w-6 h-6 ${route === path ? 'stroke-[#f3ffca]' : 'stroke-gray-500'} mt-6`}
+      />
     </Link>
   );
 }
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,13 +1,16 @@
+'use client';
 import NavIcon from '../components/NavIcon';
+import { usePathname } from 'next/navigation';
 import { House, Dumbbell, ChartNoAxesCombined, Award } from 'lucide-react';
 
 export default function SideNav() {
+  const currentPath = usePathname();
   return (
     <nav className="p-4 bg-[#141414] h-screen w-20 flex flex-col items-center justify-start pt-6">
-      <NavIcon icon={House} route="/"></NavIcon>
-      <NavIcon icon={Dumbbell} route="/WorkoutLog"></NavIcon>
-      <NavIcon icon={ChartNoAxesCombined} route="/"></NavIcon>
-      <NavIcon icon={Award} route="/"></NavIcon>
+      <NavIcon icon={House} route="/" path={currentPath} />
+      <NavIcon icon={Dumbbell} route="/WorkoutLog" path={currentPath} />
+      <NavIcon icon={ChartNoAxesCombined} route="/" path={currentPath} />
+      <NavIcon icon={Award} route="/" path={currentPath} />
     </nav>
   );
 }
